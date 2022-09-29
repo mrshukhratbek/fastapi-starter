fbcc08a2301449a99d6da6e7039ad5d0f278a987
mrshukhratbek <shukhratbekqobulov@gmail.com>
2022-09-29 19:15:24 +0500

    feat: add some folders

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 import uvicorn
 from config import APP_PORT
 
+from src.routers import users
+
 app = FastAPI()
 
-@app.get('/')
-async def root():
-  return {"message": "Hello World"}
+app.include_router(users)
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=int(APP_PORT), reload=True)
